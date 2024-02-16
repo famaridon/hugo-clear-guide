8cfff92179b9a1525e3c30ddc2a37f1e8d4821be
famaridon <famaridon@users.noreply.github.com>
2024-02-16 22:24:59 +0100

    large table must have horizontal scroll

--- assets/js/table.js
+++ assets/js/table.js
@@ -2,6 +2,13 @@
 document.addEventListener('DOMContentLoaded', function () {
     const tables = document.querySelectorAll('table');
     tables.forEach((table) => {
+        // add a parent div with table-responsive class
+        const parent = table.parentNode;
+        const wrapper = document.createElement('div');
+        wrapper.classList.add('table-responsive');
+        parent.replaceChild(wrapper, table);
+        wrapper.appendChild(table);
+        // add table classes
         table.classList.add('table', 'table-striped', 'table-bordered');
     });
 });
